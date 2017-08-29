d047bd05c88fc4b25063c64d57b0386ac95b4e35
Rasmus Roy <rasmus.roy@gmail.com>
2017-08-28 21:45:08 -0400

    Trying out chrome storage for channels

--- build/js/background.js
+++ build/js/background.js
@@ -1,18 +1,63 @@
-var channelList = ["ESL_CSGO",
-"alignftw",
-"ESL_SC2",
-"ESL_LOL",
-"ESL_Overwatch",
-"ESL_Heroes",
-"ESL_DOTA2",
-"wheeze202",
-"KingGothalion",
-"HeroHarmony"];
+// var channels = ["ESL_CSGO",
+// "alignftw",
+// "ESL_SC2",
+// "ESL_LOL",
+// "ESL_Overwatch",
+// "ESL_Heroes",
+// "ESL_DOTA2",
+// "wheeze202",
+// "KingGothalion",
+// "HeroHarmony"];
 
-// Array of channels stored in localstorage.
-localStorage.storedChannels = channelList;
-console.log(localStorage.storedChannels);
+// chrome.storage.local.set( "channels", ["ESL_CSGO",
+// "alignftw",
+// "ESL_SC2",
+// "ESL_LOL",
+// "ESL_Overwatch",
+// "ESL_Heroes",
+// "ESL_DOTA2",
+// "wheeze202",
+// "KingGothalion",
+// "HeroHarmony"] );
 
-// Splitting and creating new array of channels.
-var channels = localStorage.storedChannels.split(",");
-console.log(channels);
+
+// { "channels" : ["ESL_CSGO",
+// "alignftw",
+// "ESL_SC2",
+// "ESL_LOL",
+// "ESL_Overwatch",
+// "ESL_Heroes",
+// "ESL_DOTA2",
+// "wheeze202",
+// "KingGothalion",
+// "HeroHarmony"] }
+
+
+
+// // FUCK YEAH FETCHING 
+// function fetchTest() {
+//   fetch('https://api.twitch.tv/kraken/channels/esl_sc2?client_id=f1kmn05e2nylo6c3vvcrmt88xxd9g0')
+//     .then(function (response) {
+//       // Convert to JSON
+//       return response.json();
+//     })
+//     .then(function (j) {
+//       // Yay, `j` is a JavaScript object
+//       console.log(j);
+//     });
+// }
+
+
+// function fetchReal() {
+//   channels.forEach(function (channel) {
+//     fetch('https://api.twitch.tv/kraken/channels/' + channel + '?client_id=f1kmn05e2nylo6c3vvcrmt88xxd9g0')
+//       .then(function (response) {
+//         // Convert to JSON
+//         return response.json();
+//       })
+//       .then(function (j) {
+//         // Yay, `j` is a JavaScript object
+//         console.log(j);
+//       });
+//   })
+// }
--- build/js/index.js
+++ build/js/index.js
@@ -1,26 +1,45 @@
 // Runs functions when Extension is opened
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   channelInfoCall();
   links();
 });
 
+var channels = [];
+chrome.storage.local.get({
+  storedChannels: []
+}, function (data) {
+  channels = data.storedChannels;
+});
+
+// var channels = ["ESL_CSGO",
+// "alignftw",
+// "ESL_SC2",
+// "ESL_LOL",
+// "ESL_Overwatch",
+// "ESL_Heroes",
+// "ESL_DOTA2",
+// "wheeze202",
+// "KingGothalion",
+// "HeroHarmony"];
+
 
 // API CALL FUNCTION
 function channelInfoCall() {
-  channels.forEach(function(channel) {
+
+  channels.forEach(function (channel) {
     // Function takes in type and name and returns it with the api url.
     function streamURL(type, name) {
       // TODO: Replace XXXX with own client_id
-      return 'https://api.twitch.tv/kraken/' + type + '/' + name +'?client_id=f1kmn05e2nylo6c3vvcrmt88xxd9g0';
+      return 'https://api.twitch.tv/kraken/' + type + '/' + name + '?client_id=f1kmn05e2nylo6c3vvcrmt88xxd9g0';
       // For Config File
       // return 'https://api.twitch.tv/kraken/' + type + '/' + name +'?client_id=' + config.clientID;
     };
 
     // Initiates the function and runs once for each object in channel.
     // First tests if the stream is offline or online.
-    $.getJSON(streamURL("streams", channel), function(data) {
+    $.getJSON(streamURL("streams", channel), function (data) {
       var game,
-          status;
+        status;
 
       if (data.stream === null) {
         game = "Offline";
@@ -34,13 +53,13 @@ function channelInfoCall() {
       };
 
       // Gets the channel Data and adds the html.
-      $.getJSON(streamURL("channels", channel), function(data) {
+      $.getJSON(streamURL("channels", channel), function (data) {
         var logo = data.logo != null ? data.logo : "https://static-cdn.jtvnw.net/jtv_user_pictures/xarth/404_user_70x70.png",
-            name = data.display_name != null ? data.display_name : channel,
-            url = data.url,
-            description = status === "online" ? '' + data.status : "";
+          name = data.display_name != null ? data.display_name : channel,
+          url = data.url,
+          description = status === "online" ? '' + data.status : "";
 
-            html = `
+        html = `
             <div class="channel-element col-xs-12 ${status}">
               <div class="streamer-avatar"> <a href="${url}"><img src="${logo}"></a></div>
               <div class="streamer-info">
@@ -49,19 +68,21 @@ function channelInfoCall() {
               </div>
               <div class="streamer-title">${description}</div>
             </div>`;
-        
+
         // Sorts Streams by Online or Offline and adds it to the html.
         status === "online" ? $(".stream-container").prepend(html) : $(".stream-container").append(html)
       });
-      
+
     });
   });
 };
 
 // Function making links work in the extension
 function links() {
-  $('body').on('click', 'a', function(){
-     chrome.tabs.create({url: $(this).attr('href')});
-     return false;
-   });
-}
+  $('body').on('click', 'a', function () {
+    chrome.tabs.create({
+      url: $(this).attr('href')
+    });
+    return false;
+  });
+};
