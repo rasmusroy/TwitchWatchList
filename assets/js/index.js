a556d5db874683c8d72f483739bcc94450e24ee1
Rasmus Roy <rasmus.roy@gmail.com>
2017-09-14 22:12:31 +0200

    Added HTML and CSS for delete button

--- assets/js/index.js
+++ assets/js/index.js
@@ -74,6 +74,17 @@ function channelInfoCall() {
                 <div class="streamer-info">
                   <div class="streamer-name"> <a class="stream-link" href="${url}"${name}">${name}</a> </div>
                   <div class="streamer-game"> Streaming  <a href="https://www.twitch.tv/directory/game/${game}">${game}</a> </div>
+                  <div class="deleteBtn" id="${name}">
+                  <svg alt="Delete Channel" width="14px" height="18px" viewBox="0 0 14 18" >
+                    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                        <g id="Core" transform="translate(-299.000000, -129.000000)" fill="#706A7C">
+                            <g id="delete" transform="translate(299.000000, 129.000000)">
+                                <path d="M1,16 C1,17.1 1.9,18 3,18 L11,18 C12.1,18 13,17.1 13,16 L13,4 L1,4 L1,16 L1,16 Z M14,1 L10.5,1 L9.5,0 L4.5,0 L3.5,1 L0,1 L0,3 L14,3 L14,1 L14,1 Z" id="Shape"></path>
+                            </g>
+                        </g>
+                    </g>
+                </svg>
+              </div>
                 </div>
                 <div class="streamer-title">${description}</div>
               </div>`;
@@ -106,6 +117,7 @@ function checkForEmptyState() {
     }
   });
 }
+
 function emptyStateFunc() {
   document.getElementById("empty-state").remove();
 }
